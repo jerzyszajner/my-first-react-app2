399a57aa3bba944393ceb4777f474bc07e06b829
jerzy szajner <jerzy.szajner@gmail.com>
2024-08-17 18:55:51 +0200

    chore: reorganize project structure and refactor components
    - Moved components into a dedicated "components" directory
    - Created and styled Button, Column, Hero, List, SearchForm, and TextInput components
    - Updated App.js to utilize new component structure
    - Applied global styles and FontAwesome integration

---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,16 @@
+import styles from './SearchForm.module.scss';
+import TextInput from '../TextInput/TextInput.js';
+import Button from '../Button/Button.js'
+
+const SearchForm = () => {
+    return (
+        <div>
+            <form className={styles.searchForm}>
+                <TextInput placeholder="Search..." />
+                <Button text="Search" />
+            </form>
+        </div>
+    );
+};
+
+export default SearchForm;
